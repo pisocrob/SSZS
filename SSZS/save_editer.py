d5298050df94363c786bf2847ea1f7a88cdc360b
pisocrob <r.e.norvill@student.bradford.ac.uk>
2016-01-12 02:55:23 +0000

    First working iteration of save editor. Currently a command line script that should be run in the same folder as the save file. User should specify [echoes | fuel | supplies] as first argument and a quantity as second argument. echoes defaults to 99999. The other two default to 10.

--- SSZS/save_editer.py
+++ SSZS/save_editer.py
@@ -1,15 +1,43 @@
 import json
+import sys
 
 jsonFile = open('Autosave.json')
-jsonParsed = jsonFile.read()
+jsonData = jsonFile.read()
+parsedJson = json.loads(jsonData)
 jsonFile.close()
 backupFile = open('AutosaveBackup.json', 'w')
-backupFile.write(jsonParsed)
+backupFile.write(jsonData)
 backupFile.close()
-parsedJson = json.loads(jsonParsed)
 
-userJson = parsedJson['QualitiesPossessedList'][0]['AssociatedQualityId']
+if sys.argv[1]=='echoes':
+    for x in parsedJson['QualitiesPossessedList']:
+        if x['AssociatedQualityId']==102028:
+            if sys.argv[2]!=None:
+                x['Level']=sys.argv[2]
+            else:
+                x['level']=99999
+            jsonFile = open('Autosave.json', 'w+')
+            jsonFile.write(json.dumps(parsedJson))
+            jsonFile.close()
 
-for x in parsedJson['QualitiesPossessedList']:
-    if x['AssociatedQualityId']==102028:
-        print x
+elif sys.argv[1]=='fuel':
+    for x in parsedJson['QualitiesPossessedList']:
+        if x['AssociatedQualityId']==102027:
+            if sys.argv[2]!=None:
+                x['Level']=sys.argv[2]
+            else:
+                x['level']=10
+            jsonFile = open('Autosave.json', 'w+')
+            jsonFile.write(json.dumps(parsedJson))
+            jsonFile.close()
+
+elif sys.argv[1]=='supplies':
+    for x in parsedJson['QualitiesPossessedList']:
+        if x['AssociatedQualityId']==102026:
+            if sys.argv[2]!=None:
+                x['Level']=sys.argv[2]
+            else:
+                x['level']=10
+            jsonFile = open('Autosave.json', 'w+')
+            jsonFile.write(json.dumps(parsedJson))
+            jsonFile.close()
